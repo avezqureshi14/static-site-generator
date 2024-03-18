3c7ba844bb4b73fb82070861fbe7b32d96b4b570
avezqureshi <aveznqureshi14@gmail.com>
2024-03-18 07:57:03 +0530

    fix: form error

--- src/app/components/InAppPurchases.jsx
+++ src/app/components/InAppPurchases.jsx
@@ -11,16 +11,16 @@ const InAppPurchases = ({ inAppPurchases }) => {
                         {fieldConfig.type === "text" ?
                             <input
                                 type={fieldConfig.type}
-                                id={fieldName}
-                                name={fieldName}
+                                id={fieldConfig.title}
+                                name={fieldConfig.title}
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder={fieldConfig.placeholder}
                                 required
                             />
                             :
                             <textarea
-                                id={fieldName}
-                                name={fieldName}
+                                id={fieldConfig.title}
+                                name={fieldConfig.title}
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder={fieldConfig.placeholder}
                                 required
